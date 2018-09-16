474adb2b474f57295bad418c158dd89c02a08199
Jonathan <sellsj14@gmail.com>
2018-09-16 19:38:15 -0400

    updated the app to run staticly (i think) time to test it

--- adventure-time/src/App.js
+++ adventure-time/src/App.js
@@ -1,21 +1,31 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import CharacterCard from "./components/CharacterCard";
+import Wrapper from "./components/Wrapper";
+import friends from "./friends.json";
+import "./App.css";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
+const App = () => (
+  <Wrapper>
+    <h1 className="title">Character List</h1>
+    <CharacterCard
+      name={friends[0].name}
+      image={friends[0].image}
+      occupation={friends[0].occupation}
+      location={friends[0].location}
+    />
+    <CharacterCard
+      name={friends[1].name}
+      image={friends[1].image}
+      occupation={friends[1].occupation}
+      location={friends[1].location}
+    />
+    <CharacterCard
+      name={friends[2].name}
+      image={friends[2].image}
+      occupation={friends[2].occupation}
+      location={friends[2].location}
+    />
+  </Wrapper>
+);
 
 export default App;
