35a0622f38ccc3823a2237b8b8fcd3e719ee77a6
Jonathan <sellsj14@gmail.com>
2018-09-16 20:15:51 -0400

    the static app is working, need to convert to mapping over the objects instead of rendering each one

--- adventure-time/src/App.js
+++ adventure-time/src/App.js
@@ -1,29 +1,29 @@
 import React from "react";
 import CharacterCard from "./components/CharacterCard";
 import Wrapper from "./components/Wrapper";
-import friends from "./friends.json";
+import characters from "./characters.json";
 import "./App.css";
 
 const App = () => (
   <Wrapper>
     <h1 className="title">Character List</h1>
     <CharacterCard
-      name={friends[0].name}
-      image={friends[0].image}
-      occupation={friends[0].occupation}
-      location={friends[0].location}
+      name={characters[0].name}
+      image={characters[0].image}
+      occupation={characters[0].occupation}
+      location={characters[0].location}
     />
     <CharacterCard
-      name={friends[1].name}
-      image={friends[1].image}
-      occupation={friends[1].occupation}
-      location={friends[1].location}
+      name={characters[1].name}
+      image={characters[1].image}
+      occupation={characters[1].occupation}
+      location={characters[1].location}
     />
     <CharacterCard
-      name={friends[2].name}
-      image={friends[2].image}
-      occupation={friends[2].occupation}
-      location={friends[2].location}
+      name={characters[2].name}
+      image={characters[2].image}
+      occupation={characters[2].occupation}
+      location={characters[2].location}
     />
   </Wrapper>
 );
