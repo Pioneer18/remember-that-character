c021ff4993b47ae7f95715400a66ad9e1aeb7aa3
Jonathan <sellsj14@gmail.com>
2018-09-17 18:38:41 -0400

    added the score and topscore props to the nav, props coming from the app.js

--- adventure-time/src/components/Nav/Nav.js
+++ adventure-time/src/components/Nav/Nav.js
@@ -1,11 +1,12 @@
 import React from "react";
 import "./Nav.css";
 
-const Nav = () => (
+const Nav = props => (
     <nav class="navbar navbar-light bg-faded justify-content-between flex-nowrap flex-row">
         <a href="/" class="navbar-brand">Adventure Time Game</a>
         <ul class="nav navbar-nav flex-row">
-            <li class="nav-item"><a class="nav-link pr-3" href="">Score</a></li>
+            <li class="nav-item"><a class="nav-link pr-3" href="">Score: {props.score}</a></li>
+            <li class="nav-item"><a class="nav-link pr-3" href="">Score: {props.topScore}</a></li>
         </ul>
     </nav>
 );
