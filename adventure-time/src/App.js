3dc5944cdff65b5009588c135c62b1ecfc1dfa17
Jonathan <sellsj14@gmail.com>
2018-09-16 20:45:48 -0400

    mapping over characters for each character rendering; the wrapper, and a character card with the props of; name,image

--- adventure-time/src/App.js
+++ adventure-time/src/App.js
@@ -4,28 +4,22 @@ import Wrapper from "./components/Wrapper";
 import characters from "./characters.json";
 import "./App.css";
 
-const App = () => (
-  <Wrapper>
-    <h1 className="title">Character List</h1>
-    <CharacterCard
-      name={characters[0].name}
-      image={characters[0].image}
-      occupation={characters[0].occupation}
-      location={characters[0].location}
-    />
-    <CharacterCard
-      name={characters[1].name}
-      image={characters[1].image}
-      occupation={characters[1].occupation}
-      location={characters[1].location}
-    />
-    <CharacterCard
-      name={characters[2].name}
-      image={characters[2].image}
-      occupation={characters[2].occupation}
-      location={characters[2].location}
-    />
-  </Wrapper>
-);
+const App = () => {
+    return(
+    <React.Fragment>
+      {characters.map(character => 
+      <Wrapper>
+        <h1 className="title">Character List</h1>
+        <CharacterCard
+          name={characters[0].name}
+          image={characters[0].image}
+          occupation={characters[0].occupation}
+          location={characters[0].location}
+        />
+      </Wrapper>
+      )}
+    </React.Fragment>
+    )
+  }
 
 export default App;
