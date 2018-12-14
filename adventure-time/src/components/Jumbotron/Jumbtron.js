6929ec79225a1e05b7f62f0fc21b6cd5734a2e37
sellsj14 <sellsj14@gmail.com>
2018-12-14 13:49:16 -0500

    resizing jumbtron text again, was actually bit to small on my phone

--- adventure-time/src/components/Jumbotron/Jumbtron.js
+++ adventure-time/src/components/Jumbotron/Jumbtron.js
@@ -71,11 +71,11 @@ export default class jumbotron extends React.Component {
             <Mobile>
              <div style={height} className="jumbotron">
                     <div className="container">
-                    <span style={{fontSize: "2.4rem"}} className="top">Adventure Time Memory game</span>
+                    <span style={{fontSize: "3rem"}} className="top">Adventure Time Memory game</span>
 
                     <br/>
                     
-                    <span style={{fontSize: "1.2rem"}} className="instructions">Try to select each of the 12 characters in a row 
+                    <span style={{fontSize: "2rem"}} className="instructions">Try to select each of the 12 characters in a row 
                     
                     <br/>
                     
@@ -86,11 +86,11 @@ export default class jumbotron extends React.Component {
             <Mini>
                 <div style={height} className="jumbotron">
                     <div className="container">
-                    <span style={{fontSize: "2rem"}} className="top">Adventure Time Memory game</span>
+                    <span style={{fontSize: "2.5rem"}} className="top">Adventure Time Memory game</span>
 
                     <br/>
                     
-                    <span style={{fontSize: "1.2rem"}} className="instructions">Try to select each of the 12 characters in a row 
+                    <span style={{fontSize: "1.8rem"}} className="instructions">Try to select each of the 12 characters in a row 
                     
                     <br/>
                     
