41000694e095d3e48251b0a6085491fb1055acef
sellsj14 <sellsj14@gmail.com>
2018-12-14 13:42:54 -0500

    k

--- adventure-time/src/components/CharacterCard/CharacterCard.js
+++ adventure-time/src/components/CharacterCard/CharacterCard.js
@@ -3,8 +3,8 @@ import "./CharacterCard.css";
 import Responsive from 'react-responsive';
 const Desktop = props => <Responsive {...props} minWidth={992} />;
 const Tablet = props => <Responsive {...props} minWidth={768} maxWidth={991} />;
-const Mobile = props => <Responsive {...props} minWidth={480} maxWidth={767} />;
-const Mini = props => <Responsive {...props} maxWidth={479} />;
+const Mobile = props => <Responsive {...props} minWidth={482} maxWidth={767} />;
+const Mini = props => <Responsive {...props} maxWidth={481} />;
 
 const CharacterCard = props => (
   <React.Fragment>
